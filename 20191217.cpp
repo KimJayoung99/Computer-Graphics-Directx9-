8a29cc77cc48785f94958c4c06f199b1f0716c05
KimJayoung99 <37374215+KimJayoung99@users.noreply.github.com>
2019-12-19 22:38:08 +0900

    Add files via upload

--- 20191217.cpp
+++ 20191217.cpp
@@ -18,9 +18,10 @@
 #define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
 #define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
 
-#define ENEMY_NUM 6 
-#define BULLET_NUM 10
+#define ENEMY_NUM 10
+#define BULLET_NUM 100
 
+bool KeyCK = false;
 
 // include the Direct3D Library file
 #pragma comment (lib, "d3d9.lib")
@@ -32,7 +33,6 @@ LPDIRECT3DDEVICE9 d3ddev;    // the pointer to the device class
 LPD3DXSPRITE d3dspt;    // the pointer to our Direct3D Sprite interface
 
 
-
 // sprite declarations
 LPDIRECT3DTEXTURE9 sprite;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_hero;    // the pointer to the sprite
@@ -40,7 +40,10 @@ LPDIRECT3DTEXTURE9 sprite_enemy;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_bullet;    // the pointer to the sprite
 LPDIRECT3DTEXTURE9 sprite_back;    // the pointer to the sprite
 
-
+LPD3DXFONT dxfont;    // the pointer to the font object
+int health = 300;
+int maxhealth = 1000;
+LPDIRECT3DTEXTURE9 DisplayTexture;    // the pointer to the texture
 
 // function prototypes
 void initD3D(HWND hWnd);    // sets up and initializes Direct3D
@@ -50,6 +53,10 @@ void cleanD3D(void);		// closes Direct3D and releases memory
 void init_game(void);
 void do_game_logic(void);
 
+void load_display();
+void draw_display();
+void LoadTexture(LPDIRECT3DTEXTURE9* texture, LPCTSTR filename);
+void DrawTexture(LPDIRECT3DTEXTURE9 texture, RECT texcoords, float x, float y, int a);
 
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
@@ -124,10 +131,15 @@ void Hero::move(int i)
 // 적 클래스 
 class Enemy :public entity {
 
+	
 public:
 	void fire();
 	void init(float x, float y);
 	void move();
+	bool show();
+	void active();
+	void hide();
+	bool eShow;
 
 };
 
@@ -142,11 +154,26 @@ void Enemy::init(float x, float y)
 
 void Enemy::move()
 {
-	x_pos -= 1;
+	x_pos += 3;
 
 }
 
+bool Enemy::show()
+{
+	return eShow;
+}
 
+void Enemy::active()
+{
+	eShow = true;
+
+}
+
+void Enemy::hide()
+{
+	eShow = false;
+
+}
 
 
 
@@ -191,7 +218,7 @@ void Bullet::active()
 
 void Bullet::move()
 {
-	x_pos += 8;
+	x_pos -= 8;
 }
 
 void Bullet::hide()
@@ -325,7 +352,7 @@ void initD3D(HWND hWnd)
 	D3DXCreateSprite(d3ddev, &d3dspt);    // create the Direct3D Sprite object
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
-		L"background.png",    // the file name
+		L"woods.png",    // the file name
 		D3DX_DEFAULT,    // default width
 		D3DX_DEFAULT,    // default height
 		D3DX_DEFAULT,    // no mip mapping
@@ -356,7 +383,7 @@ void initD3D(HWND hWnd)
 
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
-		L"santa girl_.png",    // the file name
+		L"attack.png",    // the file name
 		D3DX_DEFAULT,    // default width
 		D3DX_DEFAULT,    // default height
 		D3DX_DEFAULT,    // no mip mapping
@@ -370,8 +397,24 @@ void initD3D(HWND hWnd)
 		NULL,    // not using 256 colors
 		&sprite_hero);    // load to sprite
 
+	/*D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
+		L"attack.png",    // the file name
+		D3DX_DEFAULT,    // default width
+		D3DX_DEFAULT,    // default height
+		D3DX_DEFAULT,    // no mip mapping
+		NULL,    // regular usage
+		D3DFMT_A8R8G8B8,    // 32-bit pixels with alpha
+		D3DPOOL_MANAGED,    // typical memory handling
+		D3DX_DEFAULT,    // no filtering
+		D3DX_DEFAULT,    // no mip filtering
+		D3DCOLOR_XRGB(255, 0, 255),    // the hot-pink color key
+		NULL,    // no image info struct
+		NULL,    // not using 256 colors
+		&sprite_heroAttack);    // load to sprite
+		*/
+
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
-		L"enemy.png",    // the file name
+		L"ghost1.png",    // the file name
 		D3DX_DEFAULT,    // default width
 		D3DX_DEFAULT,    // default height
 		D3DX_DEFAULT,    // no mip mapping
@@ -387,7 +430,7 @@ void initD3D(HWND hWnd)
 
 
 	D3DXCreateTextureFromFileEx(d3ddev,    // the device pointer
-		L"ball.png",    // the file name
+		L"bullet.png",    // the file name
 		D3DX_DEFAULT,    // default width
 		D3DX_DEFAULT,    // default height
 		D3DX_DEFAULT,    // no mip mapping
@@ -402,6 +445,7 @@ void initD3D(HWND hWnd)
 		&sprite_bullet);    // load to sprite
 
 
+	load_display();
 
 	return;
 }
@@ -410,19 +454,21 @@ void initD3D(HWND hWnd)
 void init_game(void)
 {
 	//객체 초기화 (처음위치)
-	hero.init(150, 300);
+	hero.init(450,250);
 
 	//적들 초기화 
 	for (int i = 0; i < ENEMY_NUM; i++)
 	{
 
-		enemy[i].init((float)(rand() % 500+500), rand() % 250+50);
+		enemy[i].init( rand()%500-400, rand() % 200 + 100);
+		enemy[i].active();
+		//enemy[i].init((float)(rand() % 500-1000), rand() % 200+50);
 	}
 
 	//총알 초기화 
 	for (int i = 0; i < BULLET_NUM; i++)
 	{
-		bullet[i].init(hero.x_pos + 50, hero.y_pos);
+		bullet[i].init(hero.x_pos - 70, hero.y_pos);
 	}
 
 }
@@ -448,40 +494,81 @@ void do_game_logic(void)
 	//적들 처리 
 	for (int i = 0; i < ENEMY_NUM; i++)
 	{
-		if (enemy[i].x_pos < 50)
-			enemy[i].init((float)(rand() % 300+300), rand() % 200 - 300); //지정위치에 랜덤으로 적 생성
+		if (enemy[i].x_pos > 650)
+		{
+			enemy[i].active();
+			enemy[i].init(rand() % 500 - 400, rand() % 200 + 100);
+		}
 		else
 			enemy[i].move();
 	}
 
+	for (int i = 0; i < 100; ++i)
+	{
+		for (int j = 0; j < ENEMY_NUM; ++j)
+		{
+			// 보이는 총알 중에
+			if (bullet[i].show() == TRUE && enemy[j].show()==TRUE)
+			{
+
+				// 충돌 되었으면
+				if (bullet[i].x_pos < enemy[j].x_pos + 55  //plus enemy x size
+					&& enemy[j].x_pos < bullet[i].x_pos + 32
+					&&bullet[i].y_pos < enemy[j].y_pos + 55
+					&& enemy[j].y_pos < bullet[i].y_pos + 32
+					)
+				{
+
+					// 충돌한 총알은 안보이고, 게이지 깍기
+
+					bullet[i].hide();
+					enemy[j].hide();
+					
+				}
+
+			}
+		}
+
+	}
+
 
 	//총알 처리 
-	for (int i = 0; i < BULLET_NUM; i++)
-	{
 		//if (bullet[i].show() == false)
 		//{
-			if (KEY_DOWN(VK_SPACE))
+	if (KeyCK == false)
+	{
+		KeyCK = true;
+		if (KEY_DOWN(VK_SPACE))
+		{
+			for (int i = 0; i < 100; ++i)
 			{
-				bullet[i].active();
-				bullet[i].init(hero.x_pos, hero.y_pos);
-				bullet[i].show() == true;
+				if (bullet[i].show() == false)
+				{
+					bullet[i].active();
+					bullet[i].init(hero.x_pos, hero.y_pos);
+					break;
+				}
 			}
-		//}
-		//else if (bullet[i].show() == true)
-		//{
-			if (bullet[i].x_pos > 640)
-			{
-				bullet[i].hide();
-				bullet[i].show() == false;
-
-			}
-
-			else
-				bullet[i].move();
-		//}
+		}
+	}
+	if (KEY_UP(VK_SPACE))
+	{
+		KeyCK = false;
 	}
 	
 
+	for (int i = 0; i < 100; ++i)
+	{
+		if (bullet[i].x_pos < 30)
+		{
+			bullet[i].hide();
+		}
+
+		else
+			bullet[i].move();
+	}
+
+	
 }
 
 // this is the function used to render a single frame
@@ -494,16 +581,8 @@ void render_frame(void)
 
 	d3dspt->Begin(D3DXSPRITE_ALPHABLEND);    // // begin sprite drawing with transparency 배경 투명하게
 
-	//UI 창 렌더링 
-
-
-	
-	static int frame = 8;    // start the program on the final frame
-	if(KEY_UP(VK_RIGHT)) frame=0;     // when the space key is pressed, start at frame 0
-	if(frame < 8) frame++;     // if we aren't on the last frame, go to the next frame
-
-	// calculate the x-position
-	int xpos = frame * 64 + 1; //한 프레임의 너비 +1
+	////UI 창 렌더링 
+	draw_display();
 
 //Background image
 	RECT back;
@@ -531,15 +610,26 @@ void render_frame(void)
 		{
 			RECT part1;
 			SetRect(&part1, 0, 0, 32, 32);  //bullet size=32
-			D3DXVECTOR3 center1(-50.0f, -20.0f, 0.0f);    // center at the upper-left corner  //x축, y축 순
+			D3DXVECTOR3 center1(0.0f, -50.0f, 0.0f);    // center at the upper-left corner  //x축, y축 순
 			D3DXVECTOR3 position1(bullet[i].x_pos, bullet[i].y_pos, 0.0f);    // position at 50, 50 with no depth
 			d3dspt->Draw(sprite_bullet, &part1, &center1, &position1, D3DCOLOR_ARGB(255, 255, 255, 255));
 		}
 	}
 
+	static int frame = 6;    // start the program on the final frame
+	if (KEY_UP(VK_SPACE))
+		frame = 0;  // when the space key is pressed, start at frame 0
+
+	//else if (KEY_UP(VK_LEFT)) frame = 0;
+	if (frame < 6) frame++;     // if we aren't on the last frame, go to the next frame
+
+
+	// calculate the x-position
+	int xpos = frame * 85 + 1; //한 프레임의 너비 +1
+
 	//주인공 
 	RECT part;
-	SetRect(&part, xpos, 0, xpos+64, 130); //주인공 size
+	SetRect(&part, xpos, 0, xpos+85, 120); //주인공 size
 	D3DXVECTOR3 center(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
 	D3DXVECTOR3 position(hero.x_pos, hero.y_pos, 0.0f);    // position at 50, 50 with no depth
 	d3dspt->Draw(sprite_hero, &part, &center, &position, D3DCOLOR_ARGB(255, 255, 255, 255));
@@ -549,14 +639,17 @@ void render_frame(void)
 
 	////적 
 	RECT part2;
-	SetRect(&part2, 0, 0, 64, 64);  //enemy size
+	SetRect(&part2, 0, 0, 60, 60);  //enemy size
 	D3DXVECTOR3 center2(0.0f, 0.0f, 0.0f);    // center at the upper-left corner
 
 	for (int i = 0; i < ENEMY_NUM; i++)
 	{
-
+		if (enemy[i].show() == true)
+		{
+		
 		D3DXVECTOR3 position2(enemy[i].x_pos, enemy[i].y_pos, 0.0f);    // position at 50, 50 with no depth
 		d3dspt->Draw(sprite_enemy, &part2, &center2, &position2, D3DCOLOR_ARGB(255, 255, 255, 255));
+		}
 	}
 
 
@@ -570,6 +663,32 @@ void render_frame(void)
 	return;
 }
 
+void LoadDisplay()
+{
+	//LoadTexture(&DisplayTexture, L"DisplaySprites.png");
+
+	D3DXCreateFont(d3ddev, 20, 0, FW_BOLD, 1, false, DEFAULT_CHARSET,
+		OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE,
+		L"Arial", &dxfont);
+
+	return;
+}
+
+void DrawDisplay()
+{
+	RECT Part;
+	//draw the healthbar
+	//display the bar
+	SetRect(&Part, 1, 1, 505, 12);
+	DrawTexture(DisplayTexture, Part, 11, 456, 255);
+
+	//display the health "juice"
+	SetRect(&Part, 506, 1, 507, 12);
+	for (int index = 0; index < (health * 490 / maxhealth); index++)
+		DrawTexture(DisplayTexture, Part, index + 18, 456, 255);
+
+	return;
+}
 
 // this is the function that cleans up Direct3D and COM
 void cleanD3D(void)
